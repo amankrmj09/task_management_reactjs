5e84504e266ba69666fcec49a463cdacb7bb52a9
Aman Kumar Maurya <amankrmj@gmail.com>
2026-05-21 19:58:21 +0530

    update server port in Vite configuration from 9090 to 3000

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
-        port: 9090,
+        port: 3000,
         open: true,
     }
 })
